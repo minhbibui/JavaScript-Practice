fd22be76c66cbe9973c5559def155674df385687
Minh Bui <minhbibui@gmail.com>
2022-08-12 16:56:44 -0400

    Finished second project

--- mystery-organism-starter/main.js
+++ mystery-organism-starter/main.js
@@ -13,10 +13,85 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+function pAequorFactory(specimenNum, dna){
+  return {
+    specimenNum: specimenNum,
+    dna: dna,
+    mutate() {
+      let mutatePosition = Math.floor(Math.random() * 15);
+      let changed = returnRandBase();
+      while(this.dna[mutatePosition] !== changed){
+        this.dna[mutatePosition] = changed;
+      }
+      return this.dna;
+    },
+    compareDNA(pAequor) {
+      let commonElement = 0;
+      for(let i = 0; i < pAequor.dna.length; i++){
+        if(this.dna[i] === pAequor.dna[i]){
+          commonElement = commonElement + 1;
+        }
+      }
+      let percentageCommon = commonElement / this.dna.length * 100;
+      return `specimen#${this.specimenNum} and specimen#${pAequor.specimenNum} have ${percentageCommon}% DNA in common`;
+    },
+    willLikelySurvive() {
+      let count = 0;
+      for(let i = 0; i < this.dna.length; i++){
+        if(this.dna[i] === 'C' || this.dna[i] === 'G'){
+          count = count + 1;
+        }
+      }
+      let survivePercent = count / this.dna.length;
+      if(survivePercent > 0.6){
+        return true;
+      }
+      else{
+        return false;
+      }
+    },
+    complementStrand(){
+      let complement = [];
+      for(let i = 0; i < this.dna.length; i++){
+        if(this.dna[i] === 'A'){
+          complement.push('T')
+        }
+        else if(this.dna[i] === 'T'){
+          complement.push('A');
+        }
+        else if(this.dna[i] === 'C'){
+          complement.push('G');
+        }
+        else if(this.dna[i] === 'G'){
+          complement.push('C');
+        }
+      }
+    }
+  };
+};
+
+function pAequorStudyArr(){
+  let study = []
+  for(let i = 1; i<=30; i++){
+    let findingSurvival = true;
+    while(findingSurvival){
+      let survive = pAequorFactory(i,mockUpStrand());
+      if(survive.willLikelySurvive() === true){
+        study.push(survive);
+        findingSurvival = false;
+      }
+    }
+  }
+  return study;
+}
 
 
+console.log("hello");
+let example = pAequorStudyArr()
+console.log(example);
 
-
-
-
-
+for(let i = 0; i < example.length; i++)
+{
+  console.log(example[i].specimenNum);
+  console.log(example[i].willLikelySurvive())
+}
